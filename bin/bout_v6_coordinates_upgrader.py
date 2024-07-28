f5a905bcbac41dfb5ef3a0ff8661d7c44391bb5b
tomc271 <tomc271@gmail.com>
2024-07-28 14:10:32 +0100

    [Unfinished].

--- bin/bout_v6_coordinates_upgrader.py
+++ bin/bout_v6_coordinates_upgrader.py
@@ -44,6 +44,68 @@ def main(*args, **kwargs):
 
         original = copy.deepcopy(contents)
 
+        # Replace
+        # `c->g11 = bar;`
+        # with
+        # `const auto g11 = bar;
+        # coord->setMetricTensor(ContravariantMetricTensor(g11, g22, g33, g12, g13, g23),
+        #                CovariantMetricTensor(g_11, g_22, g_33, g_12, g_13, g_23));`
+        # etc
+
+        # regex_1 = r"(\b.+\-\>|\.)g(\d\d)\s?\=\s?((.+)\1g(\d\d)(.+))+(?=;)"
+        # regex_2 = r"(\b.+\-\>|\.)g(\d\d)\s?\=\s?(.+)(?=;)"
+        #
+        # matches_1 = re.findall(regex_1, contents)
+        # matches_2 = re.findall(regex_2, contents)
+
+        # metric_component = namedtuple("MetricComponent", ["indices", "value"])
+
+        g11_pattern = r"(\b.+\-\>|\.)g11\s?\=\s?(.+)(?=;)"
+        first_g11_match = re.search(g11_pattern, contents)
+        # TODO:
+        # g11_matches = re.findall(g11_pattern, contents)
+        # if len(g11_matches) > 1:
+
+        regex_2 = r"(\b.+\-\>|\.)g(\d\d)\s?\=\s?(.+)(?=;)"
+        remaining_metric_components = re.findall(regex_2, contents[first_g11_match.end():])
+        # matches_after_g11 = [x for x in matches_2 if x.start() > first_g11_match.start()]
+        # metric_components = {
+        #     metric_component(component[1], component[2])
+        #     for component in remaining_metric_components
+        # }
+        metric_components = {
+            "g" + component[1]: component[2]
+            for component in remaining_metric_components
+        }
+
+        g11 = metric_components.get("g11", None)
+        g22 = metric_components.get("g22", None)
+        g33 = metric_components.get("g33", None)
+        g12 = metric_components.get("g12", None)
+        g13 = metric_components.get("g13", None)
+        g23 = metric_components.get("g23", None)
+
+        g_11 = metric_components.get("g_11", None)
+        g_22 = metric_components.get("g_22", None)
+        g_33 = metric_components.get("g_33", None)
+        g_12 = metric_components.get("g_12", None)
+        g_13 = metric_components.get("g_13", None)
+        g_23 = metric_components.get("g_23", None)
+
+        modified = contents
+        lines = contents.splitlines()
+
+        new_metric_setter = (
+            f"    coord->setMetricTensor(ContravariantMetricTensor({g11}, {g22}, {g33}, {g12}, {g13}, {g23}), "
+            f"CovariantMetricTensor({g_11}, {g_22}, {g_33}, {g_12}, {g_13}, {g_23}));")
+
+        metric_components_with_value = [c for c in metric_components if c is not None]
+        last_metric_component_with_value = metric_components_with_value[-1]
+        last_component_pattern = rf"(\b.+\-\>|\.){last_metric_component_with_value}\s?\=\s?(.+)(?=;)"
+        line_index = index_of_first_matching_line(last_component_pattern, lines)
+        lines.insert(line_index, new_metric_setter)
+        modified += "\n".join(lines)
+
         patterns_with_replacements = {
             r"(\-\>|\.)d(\w)\s?\=\s?(.+?\b)": r"\1setD\2(\3)",  # Replace `->dx =` with `->setDx()`, etc
             r"(\b.+\-\>|\.)d(\w)\s?\/\=\s?(.+)(?=;)": r"\1setD\2(\1d\2  / (\3))",  # Replace `foo->dx /= bar` with `foo->setDx(foo->dx() / (bar))`
@@ -54,14 +116,20 @@ def main(*args, **kwargs):
             r"(\-\>|\.)Bxy\s?(?!=)": r"\1Bxy()",  # Replace `c->Bxy` with `c->Bxy()` but not if is assignment
         }
 
-        modified = contents
         for pattern, replacement in patterns_with_replacements.items():
             modified = re.sub(pattern, replacement, modified)
 
-        boutupgrader.apply_or_display_patch(
-            filename, original, modified, args.patch_only, args.quiet, args.force
-        )
+        # boutupgrader.apply_or_display_patch(
+        #     filename, original, modified, args.patch_only, args.quiet, args.force
+        # )
         return modified
+
+
+def index_of_first_matching_line(last_component_pattern, lines):
+    search_result_for_all_lines = [re.search(last_component_pattern, line) for line in lines]
+    first_matching_line = [x for x in search_result_for_all_lines if x is not None][0].string
+    # first_matching_line = [re.findall(last_component_pattern, line) for line in lines][0]
+    return lines.index(first_matching_line)
 
 
 if __name__ == "__main__":
