676f1976490f44f5ae86ab8bda02692c4ef8844d
tomc <tomc271@gmail.com>
2025-11-10 11:05:55 +0000

    Test collection working, but fails to find runtest file

--- conftest.py
+++ conftest.py
@@ -1,14 +1,9 @@
-import pytest
-
-# CLI options (from earlier)
 def pytest_addoption(parser):
-    parser.addoption("--get-list", action="store_true", help="List runnable tests")
     parser.addoption("--make", action="store_true", help="Build tests instead of run")
+    parser.addoption("--get-list", action="store_true", help="List runnable tests")
 
-# For --get-list: Print and exit after collection
 def pytest_collection_modifyitems(config, items):
     if config.getoption("--get-list"):
         for item in items:
             print(item.nodeid)
-        config.hook.pytest_sessionfinish(session=config, exitstatus=0)
         raise SystemExit(0)
