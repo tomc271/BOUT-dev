7a3d00cae38dccf94829a263c367db0366621806
tomc <tomc271@gmail.com>
2025-12-18 16:50:42 +0000

    Modify test_fci_boundary to run via pytest
    Fails due to missing file `grid.fci.nc`.

--- tests/integrated/test-fci-boundary/test_fci_boundary.py
+++ tests/integrated/test-fci-boundary/test_fci_boundary.py
@@ -6,12 +6,16 @@
 # Cores: 2
 # only working with cmake
 # requires: False
+
+import os
+import pathlib
+import numpy as np
 from boututils.run_wrapper import launch_safe
 from boututils.datafile import DataFile
 from boutdata.collect import collect as _collect
 
-import numpy as np
 
+directory = "data"
 
 def collect(var):
     return _collect(
@@ -23,49 +27,51 @@ def collect(var):
     )
 
 
-nprocs = [1]  # , 2, 4]
-mthread = 2
+def test_fci_boundary():
+    nprocs = [1]  # , 2, 4]
+    mthread = 2
 
-directory = "data"
 
-with DataFile("grid.fci.nc") as grid:
-    MXG = grid.get("MXG", default=1)
-    xfwd = grid.read("forward_xt_prime")[MXG:-MXG]
-    xbwd = grid.read("backward_xt_prime")[MXG:-MXG]
+    this_directory = pathlib.Path(__file__).parent.absolute()
+    os.chdir(this_directory)
+    with DataFile("grid.fci.nc") as grid:
+        MXG = grid.get("MXG", default=1)
+        xfwd = grid.read("forward_xt_prime")[MXG:-MXG]
+        xbwd = grid.read("backward_xt_prime")[MXG:-MXG]
 
-nx = xfwd.shape[0]
+    nx = xfwd.shape[0]
 
-regions = {
-    "xin_fwd": xfwd < MXG,
-    "xout_fwd": xfwd > nx + MXG - 1,
-    "xin_bwd": xbwd < MXG,
-    "xout_bwd": xbwd > nx + MXG - 1,
-}
-regions = {k: v.astype(int) for k, v in regions.items()}
+    regions = {
+        "xin_fwd": xfwd < MXG,
+        "xout_fwd": xfwd > nx + MXG - 1,
+        "xin_bwd": xbwd < MXG,
+        "xout_bwd": xbwd > nx + MXG - 1,
+    }
+    regions = {k: v.astype(int) for k, v in regions.items()}
 
-# for x in "xout", "xin":
-#     regions[x] = np.logical_or(regions[f"{x}_fwd"], regions[f"{x}_bwd"])
-# for x in "fwd", "bwd":
-#     regions[x] = np.logical_or(regions[f"xin_{x}"], regions[f"xout_{x}"])
-# regions["all"] = np.logical_or(regions["xin"], regions["xout"])
-for x in "xout", "xin":
-    regions[x] = regions[f"{x}_fwd"] + regions[f"{x}_bwd"]
-for x in "fwd", "bwd":
-    regions[x] = regions[f"xin_{x}"] + regions[f"xout_{x}"]
-regions["all"] = regions["xin"] + regions["xout"]
+    # for x in "xout", "xin":
+    #     regions[x] = np.logical_or(regions[f"{x}_fwd"], regions[f"{x}_bwd"])
+    # for x in "fwd", "bwd":
+    #     regions[x] = np.logical_or(regions[f"xin_{x}"], regions[f"xout_{x}"])
+    # regions["all"] = np.logical_or(regions["xin"], regions["xout"])
+    for x in "xout", "xin":
+        regions[x] = regions[f"{x}_fwd"] + regions[f"{x}_bwd"]
+    for x in "fwd", "bwd":
+        regions[x] = regions[f"xin_{x}"] + regions[f"xout_{x}"]
+    regions["all"] = regions["xin"] + regions["xout"]
 
-for nproc in nprocs:
-    cmd = "./get_par_bndry"
+    for nproc in nprocs:
+        cmd = "./get_par_bndry"
 
-    # Launch using MPI
-    _, out = launch_safe(cmd, nproc=nproc, mthread=mthread, pipe=True)
+        # Launch using MPI
+        _, out = launch_safe(cmd, nproc=nproc, mthread=mthread, pipe=True)
 
-    for k, v in regions.items():
-        # Collect data
-        data = collect(f"field_{k}")
-        assert np.allclose(data, v), (
-            k + " does not match",
-            np.sum(data),
-            np.sum(v),
-            np.max(data),
-        )
+        for k, v in regions.items():
+            # Collect data
+            data = collect(f"field_{k}")
+            assert np.allclose(data, v), (
+                k + " does not match",
+                np.sum(data),
+                np.sum(v),
+                np.max(data),
+            )
