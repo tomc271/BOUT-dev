1d80258b98c1c9fa8a9248d869f634c6eef2d0f5
tomc <tomc271@gmail.com>
2026-01-05 13:17:03 +0000

    Restore CMake copying `runtest` files
    Those that are python scripts have been renamed but others, such as shell script remain.

--- cmake/BOUT++functions.cmake
+++ cmake/BOUT++functions.cmake
@@ -222,6 +222,10 @@ function(bout_add_integrated_or_mms_test BUILD_CHECK_TARGET TESTNAME)
     string(REPLACE "-" "_" TEST_FILENAME "${TEST_FILENAME}")
 	bout_copy_file(${TEST_FILENAME})
 
+	if (EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/runtest)
+        bout_copy_file(runtest)
+    endif()
+
   else()
     add_test(NAME ${TESTNAME} COMMAND ${TESTNAME} ${BOUT_TEST_OPTIONS_TESTARGS})
   endif()
